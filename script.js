989a00ffeeb046647492f9c35908ff8d38dd08c0
atlem <atle.mjolhus@live.no>
2025-02-17 12:46:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -38,28 +38,8 @@ document.addEventListener("DOMContentLoaded", function () {
                 option.textContent = mode;
                 travelSelect.appendChild(option);
             });
-
-            startSlideshow();
         });
 });
-
-function selectDestination(name) {
-    document.getElementById("destination").value = name;
-}
-
-function startSlideshow() {
-    let index = 0;
-    const slides = document.querySelectorAll(".slide");
-    
-    function showSlide() {
-        slides.forEach(slide => slide.style.display = "none");
-        slides[index].style.display = "block";
-        index = (index + 1) % slides.length;
-    }
-    
-    showSlide();
-    setInterval(showSlide, 3000);
-}
 
 function calculatePrice() {
     fetch("data.json")
